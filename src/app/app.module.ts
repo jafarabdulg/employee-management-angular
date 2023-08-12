5fbfa31af0c9a3a9d56cde83050fecafbe4b3b67
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-08-12 11:51:26 +0700

    login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './page/login/login.component';
+import {NgOptimizedImage} from "@angular/common";
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import {RouterLink} from "@angular/router";
+import { EmployeeListComponent } from './component/employee-list/employee-list.component';
+import { PagingNavComponent } from './shared/paging-nav/paging-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    EmployeeListComponent,
+    PagingNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
